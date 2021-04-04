0c608fdff7a317cc9cc04ddf8e3b5b27d62102a2
Bruno Amorim <bruno.amorim032@gmail.com>
2021-04-04 17:38:48 -0300

    electron package

--- main.js
+++ main.js
@@ -3,13 +3,16 @@ const path = require('path')
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 250,
+    height: 310,
+    frame: false,
+    resizable: false,
+    fullscreenable: false,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
-
+  // win.loadURL('https://support.microsoft.com/pt-br/windows/gestos-do-touchpad-para-windows-10-a9d28305-4818-a5df-4e2b-e5590f850741')
   win.loadFile('index.html')
 }
 
